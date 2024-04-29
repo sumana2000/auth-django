be9dfbe3a79a56b481b27c4129b56606ba6a665c
Sumana Sardar <sumana7work@gmail.com>
2024-04-29 05:30:07 +0530

    users app files updated

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import register_view, login_view
 
 urlpatterns = [
-    
+    path('register/', register_view),
+    path('login/', login_view)    
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def register_view(request):
+    try:
+        serializer_obj = UserSerializer(data=request.data)
+        serializer_obj.is_valid(raise_exception=True)
+        serializer_obj.save()
+        print(serializer_obj.data)
+        return Response(serializer_obj.data)
+    except Exception as e:
+        return Response(data=str(e),status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_view(request):
+    try:
+        pass
+    except Exception as e:
+        return Response(data=str(e),status=status.HTTP_400_BAD_REQUEST)
+

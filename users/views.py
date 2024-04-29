132be53199591ade811c19a6c8a20c34153be355
Sumana Sardar <sumana7work@gmail.com>
2024-04-30 02:00:52 +0530

    login view updated

--- users/views.py
+++ users/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
+from .models import User
+import jwt, datetime
 
 @api_view(['POST'])
 def register_view(request):
@@ -18,7 +21,30 @@ def register_view(request):
 @api_view(['POST'])
 def login_view(request):
     try:
-        pass
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+        print((user))
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        if not user.check_password(password):
+            raise AuthenticationFailed('Wrong password')
+        
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.now(datetime.timezone.utc)
+
+        }
+
+        token = jwt.encode(payload, 'secret key', algorithm='HS256')
+
+        response = Response(data={'jwt_token':token})
+
+        response.set_cookie(key='jwt_token', value=token, httponly=True) #httponly bcz cookies shouldn't be used by frontend, its only for passing b/w front and backend
+        
+        return response
+
     except Exception as e:
         return Response(data=str(e),status=status.HTTP_400_BAD_REQUEST)
 

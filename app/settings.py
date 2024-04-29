14edce0d4b7cfc24dd23efccd6e5ae9e6a979f6e
Sumana Sardar <sumana7work@gmail.com>
2024-04-29 05:31:51 +0530

    project settings & urls update

--- app/settings.py
+++ app/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
@@ -119,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+

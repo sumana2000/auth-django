b290e2e27d59d0c2e13369782bfbfaaffa93c75b
Sumana Sardar <sumana7work@gmail.com>
2024-05-01 02:56:34 +0530

    app settings modified

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'corsheaders',
     'users'
 ]
@@ -40,7 +39,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True #enables us to send cookies
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
